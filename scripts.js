40d894ff7585ad311affe90e21235e72ff638c69
prashanth <prashanthjbabu@gmail.com>
2013-06-25 21:15:56 +0530

    bootstrapping fixing urls

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-<script>
 function search()
 {
   var usn=document.getElementById("usn");
@@ -90,4 +89,3 @@ $(document).ready(function()
     document.getElementById("myDiv").innerHTML="";
   });
 });
-</script>
